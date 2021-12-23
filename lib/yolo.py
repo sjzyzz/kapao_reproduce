4b7038114a1a79015e98052f6494fe296242be7c
sjzyzz <yzz20@mails.tsinghua.edu.cn>
2021-12-23 19:47:09 +0800

    building model now, trying to have a deeper insight of model, which means know each part of the model and its usage

--- lib/yolo.py
+++ lib/yolo.py
@@ -8,6 +8,9 @@ import torch.nn as nn
 from utils.torch_utils import time_sync, scale_img, initialize_weights
 from utils.plots import feature_visualization
 from utils.autoanchor import check_anchor_order
+from models.common import *
+
+from general import make_divisible
 
 
 try:
@@ -201,5 +204,49 @@ def parse_model(d, ch):
 
     layers, save, c2 = [], [], ch[-1]
     for i, (f, n, m, args) in enumerate(d['backbone'] + d['head']): # from, number, module, args
+        # TODO: idn why use this code
+        m = eval(m) if isinstance(m, str) else m
+        for j, a in enumerate(args):
+            try:
+                args[j] = eval(a) if isinstance(a, str) else a
+            except:
+                pass
         
-    
+        # TODO: idn what this code doing
+        n = n_ = max(round(n * gd), 1) if n > 1 else n
+        if m in [Conv, GhostConv, Bottleneck, GhostBottleneck, SPP, SPPF, DWConv, MixConv2d, Focus, CrossConv, BottleneckCSP, C3, C3TR, C3SPP, C3Ghost]:
+            c1, c2 = ch[f], args[0]
+            if c2 != no:
+                # TODO: idn why this code
+                c2 = make_divisible(c2 * gw, 8)
+            
+            args = [c1, c2, *args[1:]]
+            if m in [BottleneckCSP, C3, C3TR, C3Ghost]:
+                args.insert(2, n)
+                n = 1
+            elif m is nn.BatchNorm2d:
+                args = [ch[f]]
+            elif m is Concat:
+                c2 = sum([ch[x] for x in f])
+            elif m is Detect:
+                args.append([ch[x] for x in f])
+                if isinstance(args[1], int):
+                    args[1] = [list(range(args[1] * 2))] * len(f)
+            elif m is Contrast:
+                c2 = ch[f] * args[0] ** 2
+            elif m is Expand:
+                c2 = ch[f] // args[0] ** 2
+            else:
+                c2 = ch[f]
+            
+            m_ = nn.Sequential(*[m(*args) for _ in range(n)]) if 1 < n else m(*args)
+            t = str(m)[8:-2].replace('__main__', '')
+            np = sum([x.numel() for x in m_.parameters()])
+            m_.i, m_.f, m_.type, m_.np = i, f, t, np
+            LOGGER.info('%3s%18s%3s%10.0f   %-20s%-30s' % (i, f, n_, np, t, args))
+            save.extend(x % i for x in ([f] if isinstance(f, int) else f) if x != -1)
+            layers.append(m_)
+            if i == 0:
+                ch = []
+            ch.append(c2)
+        return nn.Sequential(*layers), sorted(save)
